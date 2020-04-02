3182b6a696f98b3f69fe7b469e1f96fa58f32d9c
Sergio Niño <sergioanino@qdata.io>
2020-04-02 14:46:16 -0500

    [HUB-16523] Add search for reports by name, author, category and date

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpService} from '../../services/http.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public reportsList: Array<any>;
 
-  constructor() { }
+
+  constructor(
+      private http: HttpService,
+      private router: Router
+  ) { }
 
   ngOnInit() {
+    this.onLoadReports();
+    this.onLoadMultimedia();
   }
 
+  onLoadReports() {
+    this.http.get({
+      path: 'public/reports',
+      data: {
+        include: ['user', 'reportType', {
+          relation: 'reportType',
+          scope: {
+            include: ['mainCategory', 'subCategory']
+          }
+        }],
+        where: {
+          stateId: '5e068c81d811c55eb40d14d0'
+        }
+      },
+      encode: true
+    }).subscribe((resp: any) => {
+      this.reportsList = resp.body;
+    });
+  }
+
+  onLoadMultimedia() {
+    this.http.get({
+      path: 'public/contents',
+      data: {
+        include: ['lastUpdater'],
+        where: {
+          key: 'multimedia'
+        }
+      },
+      encode: true
+    }).subscribe((resp: any) => { });
+  }
+
+  redirectSelection(event) {
+    const location = event.multimediaType ? '/multimedia' : '/reports'
+    if (event && event.id) {
+      this.router.navigate([location, event.id]);
+    }
+  }
+
+  customSearchFn(term: string, item: any) {
+    term = term.toLowerCase();
+    const publishedDate = new Date(item.publishedAt);
+    const dateString = `${publishedDate.getDate()}/${publishedDate.getMonth() + 1}/${publishedDate.getFullYear()}`;
+    const name = item.name ? 'name' : 'title';
+    const author = item.name ? 'user' : 'lastUpdater';
+    return item[name].toLowerCase().indexOf(term) > -1 ||
+        item[author].name.toLowerCase().indexOf(term) > -1 ||
+        dateString.indexOf(term) > -1 ||
+        item.reportType.mainCategory.find(e => e.description.toLowerCase().indexOf(term) > -1);
+  }
 }
