0066baf2325b2588a054cdb1207990d47ad5f645
Juan Eduardo Delgado <juandelgado@qdata.io>
2020-04-02 20:35:13 -0500

    Implement multimedia views and adjust styles

--- src/app/pages/public/multimedia-detail/multimedia-detail.component.ts
+++ src/app/pages/public/multimedia-detail/multimedia-detail.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpService} from '../../../services/http.service';
+import {ActivatedRoute, PRIMARY_OUTLET, Router, UrlSegment, UrlSegmentGroup, UrlTree} from '@angular/router';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
     selector: 'app-multimedia-detail',
@@ -7,9 +9,108 @@ import {HttpService} from '../../../services/http.service';
     styleUrls: ['./multimedia-detail.component.scss']
 })
 export class MultimediaDetailComponent implements OnInit {
-    constructor(private httpService: HttpService) {
-    }
+  public content: any;
+  public contentId: string;
+  public videoId: string;
+  public type: string;
+  public relateds: any;
 
-    ngOnInit() {
+  constructor(private http: HttpService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router, private sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((params: any) => {
+      if (params.get('id')) {
+        this.contentId = params.get('id');
+        this.loadContent(this.contentId);
+      }
+    });
+  }
+
+  loadContent(contentId: string) {
+    const filter = {
+      where: {
+        id: contentId
+      },
+      include: ['files']
+    };
+    this.http.get({
+      path: `public/contents/`,
+      data: filter,
+      encode: true
+    }).subscribe((res) => {
+      this.content = res.body[0];
+      this.type = this.content && this.content.multimediaType ? this.content.multimediaType.name : null;
+
+      if (this.content && this.content.params && this.content.params.url && this.type === 'Video') {
+        this.getVideoUrl();
+      }
+
+      this.getRelated();
+    });
+  }
+
+  getRelated() {
+    const filter = {
+      where: {
+        id: {
+          inq: this.content.params ? this.content.params.relatedReports : []
+        }
+      },
+      limit: 3,
+      include: [{
+        relation: 'reportType',
+        scope: {
+          include: ['mainCategory', 'subCategory']
+        }
+      }]
+    };
+    this.http.get({
+      path: `public/reports/`,
+      data: filter,
+      encode: true
+    }).subscribe((res) => {
+      this.relateds = res.body;
+    });
+  }
+
+  getUrlParameter(url, name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    const results = regex.exec(url);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  }
+
+  getVideoUrl() {
+    let url;
+    if (this.content && this.content.params && this.content.params.url && this.type === 'Video') {
+      url = new URL(this.content.params.url);
+      if (url && url.search) {
+        this.videoId = this.getUrlParameter(url, 'v');
+        url = `http://www.youtube.com/embed/${this.videoId}?enablejsapi=1`;
+      }
     }
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  getPodcastUrl() {
+    if (this.content && this.content.params && this.content.params.url && this.type === 'Podcast') {
+      return this.sanitizer.bypassSecurityTrustResourceUrl(this.content.params.url);
+    }
+    return null;
+  }
+
+  getWebinarUrl() {
+    if (this.content && this.content.params && this.content.params.url && this.type === 'Webinar') {
+      return this.sanitizer.bypassSecurityTrustResourceUrl(this.content.params.url);
+    }
+    return null;
+  }
+
+  getCategory(reportType) {
+    return reportType && reportType.mainCategory && reportType.mainCategory.length ?
+      reportType.mainCategory[0].description : '';
+  }
 }
--- src/app/pages/public/multimedia-gallery/multimedia-gallery.component.ts
+++ src/app/pages/public/multimedia-gallery/multimedia-gallery.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import {HttpService} from '../../../services/http.service';
+import * as moment from 'moment';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-multimedia-gallery',
@@ -7,9 +11,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MultimediaGalleryComponent implements OnInit {
 
-  constructor() { }
+  public STORAGE_URL = environment.URL_API;
+  public contents: any = [];
+  public item1: any;
+  public item2: any;
+  public item3: any;
+
+  constructor(private http: HttpService, private router: Router) { }
 
   ngOnInit() {
+    this.loadContents();
+  }
+
+  go(item) {
+    const type =  item && item.multimediaType ? item.multimediaType.name : '';
+
+    if (type.toLowerCase() === 'webinar') {
+      return window.open(item.params.url, '_blank');
+    }
+    this.router.navigate(['/multimedia', item ? item.id : 'none']);
+  }
+
+  isNewContent(content: any) {
+    const diff = moment().diff(content.createdAt, 'hours');
+    return diff < 24;
+  }
+
+  public getType(content) {
+    const type =  content && content.multimediaType ? content.multimediaType.name : '';
+    return type.toUpperCase();
+  }
+
+  public getThumbSource(content) {
+    const thumb = content.files && content.files.length ? content.files.find(e => e.key === 'thumbnail') : null;
+    if (thumb) {
+      return this.STORAGE_URL + thumb.clientPath;
+    }
+    return 'assets/images/play_btn.png';
+  }
+
+  private loadContents() {
+    const filter = {
+      where: {
+        key: 'multimedia',
+        outstandingHome: true
+      },
+      include: ['files'],
+      order: 'createdAt DESC'
+    };
+    this.http.get({
+      path: `public/contents/`,
+      data: filter,
+      encode: true
+    }).subscribe((res) => {
+      this.contents = res.body;
+
+      this.item1 = this.contents.find(e => e.outstandingArea === 'area1');
+      this.item2 = this.contents.find(e => e.outstandingArea === 'area2');
+      this.item3 = this.contents.find(e => e.outstandingArea === 'area3');
+    });
   }
 
 }
