9f055dca3c04d22624794b4f372b841a1f760351
Raj Patel <rajpatel@qdata.io>
2020-04-22 21:24:39 +0530

    Change URL in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   URL_API:  'https://testhub.qdata.io/api/',
   URL_SOCKET:  'https://testhub.qdata.io/',
-  STORAGE_FILES: 'https://testhub.qdata.io/public/assets/',
+  STORAGE_FILES: 'https://testhub.qdata.io/api/public/assets/',
   DRAFT_KEY: '5e068d1cb81d1c5f29b62977',
 };
