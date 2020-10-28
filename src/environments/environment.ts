c6660a096256c363e8f76c9f8262b50f7095d275
Juan Eduardo Delgado <juandelgado@qdata.io>
2020-10-28 18:39:08 -0500

    [HOTFIX] Add sorting per category config in category view

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,13 @@
 
 export const environment = {
   production: false,
-  // URL_API:  'https://vision.davivienda.com/',
-  // URL_SOCKET:  'https://vision.davivienda.com/',
-  /// STORAGE_FILES: 'https://vision.davivienda.com/public/assets/',
+  URL_API:  'https://vision.davivienda.com/',
+  URL_SOCKET:  'https://vision.davivienda.com/',
+  STORAGE_FILES: 'https://vision.davivienda.com/public/assets/',
   DRAFT_KEY: '5e068d1cb81d1c5f29b62977',
   // URL_API:  'http://localhost:3000/',
   // URL_SOCKET:  'http://localhost:3000/',
   // STORAGE_FILES: 'http://localhost:3000/public/assets/'
-  URL_API:  'https://stg-hub.qdata.io/',
-  URL_SOCKET:  'https://stg-hub.qdata.io/',
-  STORAGE_FILES: 'https://stg-hub.qdata.io/public/assets/'
 };
 
 /*
